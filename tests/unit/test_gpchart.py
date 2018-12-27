b3d55a744e81a42a6d2dcdaebc5bacddc6f2a346
cbjazz <>
2018-12-27 20:09:25 +0900

    CI Coverage Test

--- tests/unit/test_gpchart.py
+++ tests/unit/test_gpchart.py
@@ -1,12 +1,12 @@
 import unittest
 
-from gpchart.gpchart import sum
+from gpchart.gpchart import sum_test
 
 class TestSum(unittest.TestCase):
 
     def test_init(self):
-        data = [1,2,3]
-        result = sum(data)
+        data = [1, 2, 3]
+        result = sum_test(data)
         self.assertEqual(result, 6)
 
 if __name__ == '__main__':
