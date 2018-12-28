c6fc6a91f5b6b360b9416664d06afaa88f21602b
cbjazz <>
2018-12-29 01:28:48 +0900

    init library code

--- gpchart/gpchart.py
+++ gpchart/gpchart.py
@@ -1,14 +1,148 @@
-'''
-This is written for CI Test
-'''
-def sum_test(arg):
-    '''
-    Add all elements
-    '''
-    total = 0
-    for val in arg:
-        total += val
-    return total
+"""GPChart Library
+
+@author cbjazz
+"""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+from cycler import cycler
+import json
+import logging
+
+#Define chart options excpet of matplotlib
+OPT_STYLE = 'style'
+OPT_COLOR_MAP = 'axes.color_map'
+OPT_COLOR_PALLETE = 'axes.color_pallete'
+OPT_COLOR_ALPHA = 'axes.color_alpha'
+OPT_PROP_CYCLE = 'axes.prop_cycle'
+OPT_BAR_WIDTH = 'axes.bar_width'
+OPT_BAND_COLOR = 'axes.band_color'
+OPT_BAND_COLOR_ALPHA = 'axes.band_color_alpha'
+
+non_mpl_options = [
+    OPT_STYLE,
+    OPT_COLOR_MAP,
+    OPT_COLOR_PALLETE,
+    OPT_COLOR_ALPHA,
+    OPT_BAR_WIDTH,
+    OPT_BAND_COLOR,
+    OPT_BAND_COLOR_ALPHA
+]
+
+class GpdbChart:
+    """
+    This class is the top class of other charts.
+    It defines the chart styles and some options.
+    """
+
+    option_dict = {}
+    title = ''
+
+    def __init__(self, title, options):
+        """
+        Parameters
+        ----------
+        options : str
+            Style options with json format
+        """
+        self.title = title
+        self.option_dict = json.loads(options)
+
+    def _set_style(self):
+        if OPT_STYLE in self.option_dict.keys():
+            plt.style.use(self.option_dict[OPT_STYLE])
+
+    def _set_mpl_option(self):
+        """This configs the customizing style defined from matplotlib
+
+        The detail options refers from
+            https://matplotlib.org/users/customizing.html
+        """
+        for key in self.option_dict.keys():
+            if key not in non_mpl_options:
+                try:
+                    mpl.rcParams[key] = self.option_dict[key]
+                except KeyError:
+                    logging.warning(key + " is unknown... skip")
+                except ValueError:
+                    logging.warning("%s of %s unrecognize...skip"%
+                        (self.option_dict[key], key))
+
+    def set_color_map(self, value, alpha=1.0, max_color_cnt=10):
+        """This creates scalar color map using gradient map.
+        The name of colormap refers from
+            https://matplotlib.org/examples/color/colormaps_reference.html
+
+        Parameters
+        ----------
+        value : str
+            The name of colormap
+        alpha : float, optional (default = 1.0)
+            Transparents of colors (0.0 ~ 1.0)
+         max_color_cnt: int, optional (default = 10)
+            The number of colors to vectorize
+
+        RETURNS
+        ----------
+        list : vectorized color map
+        """
+        color_map = []
+        color_norm = colors.Normalize(vmin=0, vmax=max_color_cnt)
+        scalar_map = cmx.ScalarMappable(norm=color_norm,
+                                       cmap=value)
+        color_map = [scalar_map.to_rgba(i, alpha=alpha)
+                     for i in range(max_color_cnt)]
+        return color_map
+
+    def _set_cycler(self, max_color_cnt=10):
+        """This defines the cycler on 'axes.prop_cycle' key of matplotlib.
+        Each legend of chart refers the color in this cycler sequentially.
+        In here user can define colormap or color pallete.
+        If user defined both, we prefer map to pallete.
+        @TODO: Through cycler, we can define line style, marker and etc.
+               But we only defined colors from currently version.
+
+        Parameters
+        ----------
+         max_color_cnt: int, optional (default = 10)
+            The number of colors to vectorize
+
+        """
+        color_map = []
+        alpha = 1.0
+        # Check Alpha
+        if OPT_COLOR_ALPHA in self.option_dict.keys():
+            alpha = self.option_dict[OPT_COLOR_ALPHA]
+
+        # We perfer map to pallete.
+        if OPT_COLOR_PALLETE in self.option_dict.keys():
+            color_map = [colors.to_rgba(val, alpha=alpha)
+                         for val in self.option_dict[OPT_COLOR_PALLETE]]
+        elif OPT_COLOR_MAP in self.option_dict.keys():
+            color_map = self.set_color_map(self.option_dict[OPT_COLOR_MAP],
+                                      alpha, max_color_cnt)
+        if color_map:
+            mpl.rcParams[OPT_PROP_CYCLE] = cycler(color = color_map)
+
+    def draw_chart(self, x, y, legend, sequence):
+        """This draw chart.
+        @TODO: Now this method is temporal and only use test.
+               We should implement through inheritance.
+        """
+        self._set_style()
+        self._set_mpl_option()
+        self._set_cycler()
 
 if __name__ == '__main__':
-    print(sum([1, 2, 3]))
+    options = '''{
+        "style":"bmh",
+        "figure.figsize": [12.0, 6.0],
+        "legend.loc":"upper right",
+        "lines.marker":"o",
+        "axes.color_map":"PuOr",
+        "axes.color_alpha":0.7
+    }'''
+    chart = GpdbChart('test', options)
+    chart.draw_chart([1,2,3], [1,2,3], ['A', 'A', 'A'], [1,1,1])
--- tests/unit/test_gpchart.py
+++ tests/unit/test_gpchart.py
@@ -1,13 +1,23 @@
 import unittest
 
-from gpchart.gpchart import sum_test
+from gpchart.gpchart import GpdbChart
+import matplotlib as mpl
 
 class TestSum(unittest.TestCase):
 
-    def test_init(self):
-        data = [1, 2, 3]
-        result = sum_test(data)
-        self.assertEqual(result, 6)
+    def test_options(self):
+        options = '''{
+            "style":"bmh",
+            "figure.figsize": [12.0, 6.0],
+            "legend.loc":"lower center",
+            "lines.marker":"o",
+            "axes.color_map":"PuOr",
+            "axes.color_alpha":0.7
+        }'''
+        chart = GpdbChart('test', options)
+        chart.draw_chart([1,2,3], [1,2,3], ['A', 'A', 'A'], [1,1,1])
+        assert mpl.rcParams['lines.marker']=='o' , "Marker does not set."
+        assert mpl.rcParams['legend.loc']=="lower center", "legend does not set"
 
 if __name__ == '__main__':
     unittest.main()
